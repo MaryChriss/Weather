cd238f03f8e8bca573dc5ba9ebc53d5bdaa93298
Mariana <marychris.crf@gmail.com>
2024-08-22 21:14:33 -0300

    Correção da nav bar duplicada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Layout } from "./components/Layout/Layout";
-import { Menu } from "./components/Menu/Menu";
 
 function App() {
   return (
   <>
     <Layout>
     <h1>Weather App</h1>
-    <Menu/>
     </Layout>
   </>
 )
